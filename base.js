c7f3c5eb52dd529ff96c1fee046e245450b625b8
Yash Moondhra <ymoondhra@Yashs-MacBook-Pro-3.local>
2019-03-20 16:54:08 -0700

    New amination and scroll jquery function. took 4.5 hours

--- base.js
+++ base.js
@@ -14,29 +14,30 @@ for (i = 0; i < acc.length; i++) {
 
 
 $(window).ready(function() {
-  $("#about").css("opacity", 0);
+  $("#about").css("opacity", 0.2);
+  if($(".about-img").css("display") == 'none') {
+      $("#about").css("opacity", 0.2);
+  }
+  init_minis();
 });
 
 $(window).scroll(function(){
     var heightHome = $(".home").height()/4;
     var scroll = $(window).scrollTop();
-    // if(scroll > heightHome) {
-    //   $(".home").css("opacity", heightHome/scroll);
-    // }
-    // else {
-    //   $(".home").css("opacity", 1);
-    // }
-
 
     var homeHeight = $(".home").height();
     var h = (scroll - (homeHeight/3.5))/((4*homeHeight/5)-(homeHeight/3.5));
-    $("#about").css("opacity", h);
+    console.log(h);
     $(".home").css("opacity", (1-h));
+    $("#about").css("opacity", h + 0.2);
+    if(scroll < heightHome) {
+      if(($(".about-img").css("display") == 'none')) {
+        $("#about").css("opacity", 0.2);
+      }
+      $("#about").css("opacity", 0.2);
+    }
     if(scroll > (4*homeHeight/5)) {
-      $(".home").css("opacity", 0);
+      $(".home").css("opacity", 0.2);
       $("#about").css("opacity", 1);
     }
-
-    console.log("OPACITY: : : " + $(".home").css("opacity") + "OPACITY: : : " + $("#about").css("opacity"))
-
   });
