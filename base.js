f6b207f013bd399172dd22531f3122fdb5a1d7c4
Rahul Sheth <Rahul@rahuls-mbp-2.lan>
2019-04-03 10:51:11 -0700

    Added sliding div

--- base.js
+++ base.js
@@ -13,6 +13,28 @@ for (i = 0; i < acc.length; i++) {
 }
 
 
+var second_acc = document.getElementsByClassName("hover_show_div");
+var third_acc = document.getElementsByClassName("schedule_content_div")
+var k;
+function handler_create ( param ) {
+  return function() {
+    console.log("in");
+    var next_div = third_acc[param]
+    console.log(param);
+    console.log(next_div, "Here's the div");
+    if (next_div.style.display == 'none') {
+      next_div.style.display = '';
+    } else {
+      next_div.style.display = 'none';
+    }
+  }
+}
+for (k = 0; k < second_acc.length; k++) {
+  second_acc[k].addEventListener("mouseover", handler_create(k))
+  second_acc[k].addEventListener("mouseleave", handler_create(k))
+}
+
+
 $(window).ready(function() {
   $("#about").css("opacity", 0.2);
   if($(".about-img").css("display") == 'none') {
