546cfd5cc2ba8e2ea677d44f08240e150d6e34e4
Cody Swain <swain.cody@gmail.com>
2019-04-03 11:13:25 -0700

    Fixed disappearing schedule description div

--- base.js
+++ base.js
@@ -19,6 +19,7 @@ var k;
 function handler_create ( param ) {
   return function() {
     console.log("in");
+    console.log("param", param);
     var next_div = third_acc[param]
     console.log(param);
     console.log(next_div, "Here's the div");
@@ -27,11 +28,24 @@ function handler_create ( param ) {
     } else {
       next_div.style.display = 'none';
     }
+
+    /* Clear the corresponding next divs for other elements */
+    for (k=0; k<second_acc.length+1; k++){
+      console.log("k", k);
+      console.log("param: ", param);
+      if (k != param){
+        var next_div = third_acc[k]
+        next_div.style.display = 'none';
+      } else {
+        var next_div = third_acc[k]
+        next_div.style.display = '';
+      }
+    }
   }
 }
 for (k = 0; k < second_acc.length; k++) {
   second_acc[k].addEventListener("mouseover", handler_create(k))
-  second_acc[k].addEventListener("mouseleave", handler_create(k))
+  // second_acc[k].addEventListener("mouseleave", handler_create(k))
 }
 
 
